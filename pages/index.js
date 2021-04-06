7d446bb0a29e0885dfec7a051e0419b1d34af910
Sara Vieira <hey@iamsaravieira.com>
2021-04-07 01:16:15 +0200

    add faicon

--- pages/index.js
+++ pages/index.js
@@ -40,8 +40,26 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-night-dark text-white">
       <Head>
-        <title>GLTF {"->"} React Three Fiber</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
 
       <main className="flex flex-col items-center justify-center flex-1 px-20 ">
