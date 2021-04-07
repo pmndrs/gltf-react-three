f6ec57cf7b8b28b90b2954d1ceee5aafa9d4f3c5
Paul Henschel <drcmda@gmail.com>
2021-04-07 22:41:01 +0200

    fix center and shadow

--- components/viewer.js
+++ components/viewer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import * as THREE from 'three'
 import { Suspense, useState } from 'react'
-import { Canvas, useThree } from '@react-three/fiber'
+import { Canvas, useThree, useFrame } from '@react-three/fiber'
 import { OrbitControls, Environment, ContactShadows } from '@react-three/drei'
 
 export const Stage = React.forwardRef(function Center({ children, object, ...props }, ref) {
@@ -14,7 +14,7 @@ export const Stage = React.forwardRef(function Center({ children, object, ...pro
     object.traverse((obj) => {
       if (obj.isMesh) {
         obj.castShadow = obj.receiveShadow = true
-        obj.material.envMapIntensity = 0.2
+        obj.material.envMapIntensity = 0.8
       }
     })
 
@@ -23,14 +23,18 @@ export const Stage = React.forwardRef(function Center({ children, object, ...pro
     const box3 = new THREE.Box3().setFromObject(inner.current)
     const center = new THREE.Vector3()
     const sphere = new THREE.Sphere()
+    const height = box3.max.y - box3.min.y
     box3.getCenter(center)
     box3.getBoundingSphere(sphere)
     setRadius(sphere.radius)
-    outer.current.position.set(-center.x, -center.y * 2, -center.z)
+    console.log(box3)
+    outer.current.position.set(-center.x, -center.y + height / 2, -center.z)
   }, [])
 
   React.useLayoutEffect(() => {
-    camera.position.set(0, radius * 1.5, radius * 2)
+    camera.position.set(0, radius * 1.5, radius * 2.5)
+    camera.near = 1
+    camera.far = Math.max(5000, radius * 4)
     camera.lookAt(0, 0, 0)
   }, [radius])
 
@@ -43,15 +47,14 @@ export const Stage = React.forwardRef(function Center({ children, object, ...pro
       </group>
       <ContactShadows
         rotation-x={Math.PI / 2}
-        position={[0, 0, 0]}
-        opacity={1}
+        opacity={0.5}
         width={radius * 2}
         height={radius * 2}
         blur={2}
         far={radius / 2}
       />
       <spotLight position={[radius, radius * 2, radius]} intensity={2} castShadow />
-      <pointLight position={[-radius * 2, -radius * 2, -radius * 2]} intensity={0.5} />
+      <pointLight position={[-radius * 2, -radius * 2, -radius * 2]} />
     </group>
   )
 })
