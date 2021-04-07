b6d70748c09d2066a9e4a33bffd34f9e57cc8b54
Paul Henschel <drcmda@gmail.com>
2021-04-07 16:23:41 +0200

    center model

--- components/viewer.js
+++ components/viewer.js
@@ -1,13 +1,38 @@
+import * as THREE from "three";
 import { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useThree } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+
+export const Center = React.forwardRef(function Center({ children, ...props }, ref) {
+  const camera = useThree((state) => state.camera);
+  const inner = React.useRef();
+  React.useLayoutEffect(() => {
+    if (inner.current) {
+      const box3 = new THREE.Box3().setFromObject(inner.current);
+      const center = new THREE.Vector3();
+      const sphere = new THREE.Sphere();
+      box3.getCenter(center);
+      box3.getBoundingSphere(sphere);
+      inner.current.position.set(-center.x, -center.y, -center.z);
+      camera.position.set(0, sphere.radius, sphere.radius * 3);
+      camera.lookAt(0, 0, 0);
+    }
+  }, []);
+  return (
+    <group ref={ref} {...props}>
+      <group ref={inner}>{children}</group>
+    </group>
+  );
+});
 
 const Viewer = ({ scene }) => {
   return (
-    <Canvas pixelRatio={[1, 2]} camera={{ position: [-2, 2, 4], fov: 25 }}>
+    <Canvas pixelRatio={[1, 2]} camera={{ position: [0, 0, 150], fov: 50 }}>
       <directionalLight position={[10, 10, 5]} intensity={1.5} />
       <Suspense fallback={null}>
-        <primitive scale={[0.2, 0.2, 0.2]} object={scene} />
+        <Center>
+          <primitive object={scene} />
+        </Center>
       </Suspense>
       <OrbitControls autoRotate />
     </Canvas>
