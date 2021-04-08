071662a2ffd4e19b1ada1ccd1e9a8c6f6c1573e3
Paul Henschel <drcmda@gmail.com>
2021-04-08 13:23:35 +0200

    use stage from drei

--- components/viewer.js
+++ components/viewer.js
@@ -1,68 +1,22 @@
 import React from 'react'
-import * as THREE from 'three'
-import { Suspense, useState } from 'react'
-import { Canvas, useThree } from '@react-three/fiber'
-import { OrbitControls, Environment, ContactShadows } from '@react-three/drei'
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Stage } from '@react-three/drei'
 
-function Stage({ children, ...props }) {
-  const camera = useThree((state) => state.camera)
-  const outer = React.useRef()
-  const inner = React.useRef()
-  const [radius, setRadius] = useState(0)
-
+export default function Viewer({ scene }) {
   React.useLayoutEffect(() => {
-    inner.current.traverse((obj) => {
+    scene.traverse((obj) => {
       if (obj.isMesh) {
         obj.castShadow = obj.receiveShadow = true
         obj.material.envMapIntensity = 0.8
       }
     })
-
-    outer.current.position.set(0, 0, 0)
-    outer.current.updateWorldMatrix(true, true)
-    const box3 = new THREE.Box3().setFromObject(inner.current)
-    const center = new THREE.Vector3()
-    const sphere = new THREE.Sphere()
-    const height = box3.max.y - box3.min.y
-    box3.getCenter(center)
-    box3.getBoundingSphere(sphere)
-    setRadius(sphere.radius)
-    outer.current.position.set(-center.x, -center.y + height / 2, -center.z)
-  }, [])
-
-  React.useLayoutEffect(() => {
-    camera.position.set(0, radius * 1.5, radius * 2.5)
-    camera.near = 1
-    camera.far = Math.max(5000, radius * 4)
-    camera.lookAt(0, 0, 0)
-  }, [radius])
-
-  return (
-    <group {...props}>
-      <group ref={outer}>
-        <group ref={inner}>{children}</group>
-      </group>
-      <ContactShadows
-        rotation-x={Math.PI / 2}
-        opacity={0.5}
-        width={radius * 2}
-        height={radius * 2}
-        blur={2}
-        far={radius / 2}
-      />
-      <spotLight position={[radius, radius * 2, radius]} intensity={2} castShadow />
-      <pointLight position={[-radius * 2, -radius * 2, -radius * 2]} />
-      <Environment preset="city" />
-    </group>
-  )
-}
-
-const Viewer = ({ scene }) => {
+  }, [scene])
   return (
     <Canvas shadows dpr={[1, 1.5]} camera={{ position: [0, 0, 150], fov: 50 }}>
       <ambientLight intensity={0.25} />
       <Suspense fallback={null}>
-        <Stage>
+        <Stage contactShadow shadows adjustCamera environment="city">
           <primitive object={scene} />
         </Stage>
       </Suspense>
@@ -70,5 +24,3 @@ const Viewer = ({ scene }) => {
     </Canvas>
   )
 }
-
-export default Viewer
--- utils/sandboxCode.js
+++ utils/sandboxCode.js
@@ -28,88 +28,32 @@ export const sandboxCode = ({ fileName, textOriginalFile, code, types }) => ({
     'src/index.js': {
       content: `
 import React from 'react'
-import ReactDOM from "react-dom";
-import App from "./App";
+import ReactDOM from "react-dom"
+import './style.css'
+import App from "./App"
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-    <App />,
-  rootElement
-);
-`,
+ReactDOM.render(<App />, document.getElementById("root"))`,
     },
     'src/App.js': {
       content: `
-import React from 'react'
-import './style.css'
 import * as THREE from 'three'
-import { Suspense, useState } from 'react'
+import React, { Suspense, useState } from 'react'
 import { Canvas, useThree } from '@react-three/fiber'
-import { OrbitControls, Environment, ContactShadows } from '@react-three/drei'
+import { OrbitControls, Stage } from '@react-three/drei'
 import Model from './Model'
 
-export const Stage = React.forwardRef(function Stage(props, ref) {
-  const camera = useThree((state) => state.camera)
-  const outer = React.useRef()
-  const inner = React.useRef()
-  const [radius, setRadius] = useState(0)
-
-  React.useLayoutEffect(() => {
-    outer.current.position.set(0, 0, 0)
-    outer.current.updateWorldMatrix(true, true)
-    const box3 = new THREE.Box3().setFromObject(inner.current)
-    const center = new THREE.Vector3()
-    const sphere = new THREE.Sphere()
-    const height = box3.max.y - box3.min.y
-    box3.getCenter(center)
-    box3.getBoundingSphere(sphere)
-    setRadius(sphere.radius)
-    outer.current.position.set(-center.x, -center.y + height / 2, -center.z)
-  }, [])
-
-  React.useLayoutEffect(() => {
-    camera.position.set(0, radius * 1.5, radius * 2.5)
-    camera.near = 1
-    camera.far = Math.max(5000, radius * 4)
-    camera.lookAt(0, 0, 0)
-  }, [radius])
-
+export default function Viewer() {
   return (
-    <group ref={ref} {...props}>
-      <group ref={outer}>
-        <group ref={inner}>
-          <Model />
-        </group>
-      </group>
-      <ContactShadows
-        rotation-x={Math.PI / 2}
-        position={[0, 0, 0]}
-        opacity={1}
-        width={radius * 2}
-        height={radius * 2}
-        blur={2}
-        far={radius / 2}
-      />
-      <spotLight position={[radius, radius * 2, radius]} intensity={1} castShadow />
-      <pointLight position={[-radius * 2, -radius * 2, -radius * 2]} />
-    </group>
-  )
-})
-
-const Viewer = () => {
-  return (
-    <Canvas shadows dpr={[1, 1.5]} camera={{ position: [0, 0, 150], fov: 50 }}>
+    <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 150], fov: 50 }}>
       <Suspense fallback={null}>
-        <Stage />
-        <Environment preset="warehouse" />
+        <Stage contactShadow shadows adjustCamera environment="city">
+          <Model />
+        </Stage>
       </Suspense>
       <OrbitControls autoRotate />
     </Canvas>
   )
-}
-
-export default Viewer              
-          `,
+}`,
     },
     'src/style.css': {
       content: `body,
@@ -127,8 +71,9 @@ export default Viewer
     'package.json': {
       content: {
         dependencies: {
-          '@react-three/drei': '^4.0.4',
+          '@react-three/drei': '^4.1.1',
           '@react-three/fiber': '^6.0.9',
+          '@types/three': '0.127.0',
           react: '^17.0.2',
           'react-dom': '^17.0.2',
           three: '0.127.0',
