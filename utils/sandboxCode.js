3ce772f4f39e3c4b00692cb5dc6c45f87748082b
Paul Henschel <drcmda@gmail.com>
2021-04-08 14:04:08 +0200

    cleanup csb code

--- utils/sandboxCode.js
+++ utils/sandboxCode.js
@@ -36,9 +36,8 @@ ReactDOM.render(<App />, document.getElementById("root"))`,
     },
     'src/App.js': {
       content: `
-import * as THREE from 'three'
-import React, { Suspense, useState } from 'react'
-import { Canvas, useThree } from '@react-three/fiber'
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Stage } from '@react-three/drei'
 import Model from './Model'
 
