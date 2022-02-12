20fb2ca47a657c57b3687693f1ce23a4538c386d
Sara Vieira <hey@iamsaravieira.com>
2022-02-12 01:57:27 +0100

    add plausible; remove fathom

---
+++ pages/_document.js
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+        <script defer data-domain="gltf.pmnd.rs" src="https://analytics.iamsaravieira.com/js/plausible.js"></script>
+      </Html>
+    )
+  }
+}
