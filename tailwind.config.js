94da561874855f8ef1fff59b03b50a88f7be9da6
Sara Vieira <hey@iamsaravieira.com>
2022-02-12 02:13:49 +0100

    update packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,7 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        'night-green': 'rgb(173, 219, 103)',
-        'night-dark': 'rgb(1, 22, 39)',
-      },
-    },
-  },
-  variants: {
-    extend: {
-      gridTemplateRows: {
-        autofill: 'auto 1fr',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
