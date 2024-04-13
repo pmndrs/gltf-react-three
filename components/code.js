89c9477b0b4a783e7e94a90d3d4c232734ddf79a
SaraVieira <hey@iamsaravieira.com>
2024-04-13 17:05:33 +0100

    update packages

--- components/code.js
+++ components/code.js
@@ -1,9 +1,8 @@
-import theme from 'prism-react-renderer/themes/nightOwlLight'
-import Highlight, { defaultProps } from 'prism-react-renderer'
+import  {Highlight, defaultProps, themes } from 'prism-react-renderer'
 
 const Code = ({ children }) => {
   return (
-    <Highlight {...defaultProps} theme={theme} code={children} language="jsx">
+    <Highlight {...defaultProps} theme={themes.nightOwlLight} code={children} language="jsx">
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
         <pre
           className={`${className} whitespace-pre-wrap col-span-3 p-16 overflow-auto bg-white h-full`}
